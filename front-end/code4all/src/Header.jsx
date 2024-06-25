c5cf939266b11658773ee211c248815f8a101324
Ruby Isabelle Oliveira <rubyoliveira@meta.com>
2024-06-25 13:35:02 -0700

    starting base

---
+++ front-end/code4all/src/Header.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function Header() {
+
+  return (
+    <div className = "header-container">
+        <h1>Code4All</h1>
+        <button>Log In</button>
+        <button>Sign Up</button>
+    </div>
+
+  )
+}
+
+export default Header
